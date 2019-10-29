cbd1cf1c0b30d0af9207a77c8ca22aabe81353ce
catdance124 <kinosi.cat@gmail.com>
2019-10-29 20:28:25 +0900

    超えたときに選択不可能・色付けなど

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from flask import Flask, render_template
 import pandas as pd
+from settings import weight_limit, capacity_limit
+
 app = Flask(__name__)
 df = pd.read_csv('items.csv', encoding="shift-jis")
-print(df)
 
 @app.route("/")
 def hello():
@@ -14,7 +15,7 @@ def index():
     items = []
     for index, row in df.iterrows():
         items.append({'id':row['id'], 'name':row['name'], 'weight':row['weight'], 'capacity':row['capacity'], 'value':row['value']})
-    return render_template('index.html', items=items)
+    return render_template('index.html', items=items, weight_limit=weight_limit, capacity_limit=capacity_limit)
 
 
 if __name__ == "__main__":
---
+++ settings.py
@@ -0,0 +1,2 @@
+weight_limit = 5
+capacity_limit = 15
